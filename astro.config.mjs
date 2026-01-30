77b24d46ae28fef9bac91e9cf7a92eb1b29bbad3
Anton Kulchytskyi <antek1809@gmail.com>
2026-01-30 20:32:12 +0200

    chore(seo): enable sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { fileURLToPath, URL } from 'url';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://uplevelcorp.ca',
+  integrations: [sitemap()],
   vite: {
     resolve: {
       alias: {
